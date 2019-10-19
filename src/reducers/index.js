df468df4a67b14c7fa253ae9b3faae54ad65c7d3
micah riley <micahriley88@gmail.com>
2019-10-18 20:11:34 -0400

    Created reducer with basic switch case with default case that returns state(required). Will be referencing imported actions for each case and how they affect the state.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,3 +5,12 @@ const initialState = {
     isFetching: false,
     error: ""
 };
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
