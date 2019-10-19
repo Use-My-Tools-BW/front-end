62cff1567bd172c12848e26369fc3c0d1988ed44
micah riley <micahriley88@gmail.com>
2019-10-18 22:07:21 -0400

    Fixed import for axiosWithAuth inside the actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import axiosWithAuth from "../utils/axiosWithAuth";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export const START_FETCHING = "START_FETCHING";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import rootReducer from "./reducers"
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
