0d065df8aa956d5e9347adce281f762451ae2454
micah riley <micahriley88@gmail.com>
2019-10-18 22:11:48 -0400

    Added redux state into reference at parent component (index.js) and wrapping app.js-containing all child components- in provider to give access to redux state and actions.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,14 @@ import { Provider } from "react-redux";
 
 import rootReducer from "./reducers"
 
+const store = createStore(rootReducer);
+console.log(store, "Redux Store's initial State")
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
