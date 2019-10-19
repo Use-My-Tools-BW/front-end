eacc275e84d0455e69e59ef7e81dc621fe27c0b0
micah riley <micahriley88@gmail.com>
2019-10-18 20:18:44 -0400

    Created remaining cases for reducer's switch statement. Each case affects the redux store accordingly in terms of data, status, and endpoint response from the actions response/payload.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,6 +8,25 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case START_FETCHING:
+            return {
+                ...state,
+                isFetching: true,
+                error: ""
+            }
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                error: "",
+                user: action.payload
+            }
+        case FETCH_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
         default:
             return state;
     }
