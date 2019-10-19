4059fa5db39ce805eef1a95c48eff2782d410df5
micah riley <micahriley88@gmail.com>
2019-10-18 21:59:56 -0400

    Added action fetchUserData() that sends the response as payload and gets set to users: [] state.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
 import axios from "axios"
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 export const START_FETCHING = "START_FETCHING";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAILURE";
+
+export const fetchUserData = () => dispatch => {
+    dispatch({ type: START_FETCHING });
+    axiosWithAuth()
+    // Change to .get parameter to endoing that returns user's data based on token being passed in header
+    .get("http://url:port/api/users")
+    .then(res => dispatch({ type: FETCH_SUCCESS, payload: res }) & console.log(res, "Data returned from fetchUserData action and set to state."))
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response }))
+}
