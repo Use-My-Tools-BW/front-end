f9cd94ee3a1e167afd7fbdaa3e024129ede4734d
micah riley <micahriley88@gmail.com>
2019-10-20 16:36:07 -0400

    Used connect() on Home component to state store. Added redux reducer fetchToolListings add the redux state to props (mapStateToProps). Imported and added Home component to App.js return

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,25 @@
-// App's Home Page
+import React from "react";
+import { connect } from "react-redux";
+
+import { fetchToolListings } from "../actions";
+
+function Home(props) {
+
+    return (
+        <>
+        </>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        tools: state.tools,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+  };
+
+export default connect(
+    mapStateToProps,
+    { fetchToolListings }
+)(Home)
