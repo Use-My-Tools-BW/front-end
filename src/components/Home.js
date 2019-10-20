13c85107934df3e571539d2acd58b01f801a9cb3
micah riley <micahriley88@gmail.com>
2019-10-20 18:26:37 -0400

    Added basic layout with background colors to depict div positioning. All elements with a green background will likely be removed/replaced with components.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,30 @@ function Home(props) {
 
     return (
         <>
+            <div className="hero-search" style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}>
+                <form>
+                    {/* handleChange and onSubmit will need to be added */}
+                    <input type="text"/>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+            <div className="featured-products-categories" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <div className="featured-products" style={{ height: '65vh', width: '80vw', paddingTop: 25, backgroundColor: 'red', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+                    {/* Replace divs below with card components getting props from state passed down */}
+                    <div style={{ height: '45%', width: '45%', backgroundColor: 'green' }} />
+                    <div style={{ height: '45%', width: '45%', backgroundColor: 'green' }} />
+                    <div style={{ height: '45%', width: '45%', backgroundColor: 'green' }} />
+                    <div style={{ height: '45%', width: '45%', backgroundColor: 'green' }} />
+                </div>
+                <div className="categories" style={{ height: '35vh', width: '80vw', backgroundColor: 'blue', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    {/* Slider for categories will be added here. Most likely react-id-swiper (see npm) */}
+                    <div className="slider" style={{ height: '30vh', width: '90%', backgroundColor: 'green' }} />
+                </div>
+            </div>
+            <div className="home-button-icons" style={{ height: '33vh', width: '100%', backgroundColor: 'red', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                {/* Account, Borrow, Lend icon buttons mapped here */}
+                <div className="icons" style={{ width: '75vw', height: '30vh', backgroundColor: 'green' }} />
+            </div>
         </>
     )
 }
