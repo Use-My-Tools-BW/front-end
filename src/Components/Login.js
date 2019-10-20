f022eb3a450adb47680eab18751fecbc3a333fb4
Steve Renner <che1111@hotmail.com>
2019-10-20 13:33:23 -0700

    Login skeleton

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,3 +1,58 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { axiosWithAuth } from "./utils/axiosWithAuth"
+import { Form, Message } from 'semantic-ui-react';
+
+const Login = props => {
+    const [login, setLogin] = useState({username: "", password: ""})
+
+    const handleChange = e => {
+        setLogin({...login, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setLogin({username: "", password: ""})
+
+        axiosWithAuth().post("/login", login)
+        .then(res => {
+            console.log(res)
+            localStorage.setItem("token", res.data.token)
+            localStorage.setItem("id", res.data.id)
+
+            //if, else to route proper login (props)
+        })
+        .catch(err => console.log(err))
+        setLogin({username: "", password: ""})
+    }
+
+    return (
+        <div>
+            <h1>Use My Tools</h1>
+            <Form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                name="username"
+                value={login.username}
+                onChange={handleChange}
+                placeholder="Username"
+                />
+                <input
+                type="password"
+                name="password"
+                value={login.password}
+                onChange={handleChange}
+                placeholder="Password"
+                />
+                <button type="submit">Log In</button>
+            </Form>
+            <div>
+                <Message>
+                    Don't have an account?  <Link to='/register'>Register Now</Link>
+                </Message>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
