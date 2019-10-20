4a55cdc6901e749c5ca729354522f56f8eebfd74
micah riley <micahriley88@gmail.com>
2019-10-20 00:44:44 -0400

    Finished fetchLoggedUser action so far.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 export const START_FETCHING = "START_FETCHING";
 
 export const FETCH_TOOLLIST_SUCCESS = "FETCH_TOOLLIST_SUCCESS";
-export const FETCH_LOGIN_SUCCES = "FETCH_LOGIN_SUCCES";
+export const FETCH_LOGIN_SUCCESS = "FETCH_LOGIN_SUCCESS";
 
 export const FETCH_FAILURE = "FETCH_FAILURE";
 
@@ -14,5 +14,13 @@ export const fetchToolListings = () => dispatch => {
     // Change to .get parameter to endpoint that returns user's data based on token being passed in header
     .get("http://url:port/api/users")
     .then(res => dispatch({ type: FETCH_TOOLLIST_SUCCESS, payload: res }) & console.log(res, "Data returned from fetchToolListings action and set to state."))
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response }))
+}
+export const fetchLoggedUser = (id) => dispatch => {
+    // fetchLoggedUser is invoked with id
+    dispatch({ type: START_FETCHING });
+    axiosWithAuth()
+    .get(`http://url:port/api/user${id}`)
+    .then(res => dispatch({ type: FETCH_LOGIN_SUCCESS, payload: res }) & console.log(res, "Data returned from fetchLoggedUser action and set to state."))
     .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response }))
 }
