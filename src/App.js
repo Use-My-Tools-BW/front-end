557e145b38cf164534a5f4623046ac88c66ef948
Steve Renner <che1111@hotmail.com>
2019-10-20 17:22:23 -0700

    Routes - Login/Sign up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Login}/>
+      <Route exact path="/register" component={RegisterForm}/>
     </div>
   );
 }
