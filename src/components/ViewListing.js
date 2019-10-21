5a4e291e562996301a8bd9049ed7a61ffb5e63bf
micah riley <micahriley88@gmail.com>
2019-10-21 00:03:18 -0400

    Created empty ViewListing functional >> component card/modal

--- src/components/ViewListing.js
+++ src/components/ViewListing.js
@@ -1,1 +1,11 @@
-// Individual Tool Post View From Tool List Page
+import React from 'react';
+// This will be what's used to view tools from the featured tools/search results
+
+function ViewListing() {
+    return(
+        <>
+        </>
+    )
+}
+
+export default ViewListing;
