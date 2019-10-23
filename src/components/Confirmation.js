da691157e8b15cd70058ad1cf6889e72716fafe7
Steve Renner <che1111@hotmail.com>
2019-10-23 07:54:53 -0700

    Adding Cards to Confirmation Page

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,6 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 function Confirmation(props) {
 
     const [confirmation, setConfirmation] = useState([]);
@@ -35,7 +44,7 @@ function Confirmation(props) {
     }
 
     // delete functionality here?
-    
+
     return(
         <>
             <h2>Confirmation</h2>
@@ -43,20 +52,36 @@ function Confirmation(props) {
                 <div className="left-column" style={{ height: '100vh', width: '50%', backgroundColor: 'blue', display: 'flex', alignItems: 'center', justifyContent: 'space-around', flexDirection: 'column'  }}>
                     <div className="product-image" style={{ width: '50%', height: '25%', backgroundColor: 'red', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}}>
                         {/* below will be where the product image shows  */}
-                        <div style={{ height: '80%', width: '80%', backgroundColor: 'green' }} />
+                        <Card style={{ height: '80%', width: '80%', backgroundColor: 'green' }}>
+                            <CardContent>
+                                <h3>Tool Image</h3>
+                            </CardContent>
+                        </Card>
                     </div>
                     <div className="product-details" style={{ width: '50%', height: '30%', backgroundColor: 'red', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}}>
                         {/* below will be where the product details(name, model, etc) will show*/}
-                        <div style={{ height: '80%', width: '80%', backgroundColor: 'green' }} />
+                        <Card Style={{ height: '80%', width: '80%', backgroundColor: 'green' }}>
+                            <CardContent>
+                                <h3>Product Details</h3>
+                            </CardContent>
+                        </Card>
                     </div>
                     <div style={{ height: '30%', width: '30%', backgroundColor: 'red', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}} >
                         {/* Dollar Amount / Price will replace div below */}
-                        <div className="price" style={{ height: '50%', width: '50%', backgroundColor: 'green' }} />
+                        <Card style={{ height: '50%', width: '50%', backgroundColor: 'green' }}>
+                            <CardContent>
+                                <h3>Total Price</h3>
+                            </CardContent>
+                        </Card>
                     </div>
                 </div>
                 <div className="confirmed-details" style={{ width: '40%', height: '80%', backgroundColor: 'red', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}}>
                     {/* Product details(description) involved account details shown below */}
-                    <div style={{ height: '80%', width: '80%', backgroundColor: 'green' }} />
+                    <Card style={{ height: '80%', width: '80%', backgroundColor: 'green' }}>
+                        <CardContent>
+                            <h3>Payment Checkout</h3>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         </>
